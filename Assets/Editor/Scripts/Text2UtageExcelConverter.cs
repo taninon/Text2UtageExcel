f8aaa41fbc871a35910ffcc24766158fe575a244
noriaki_tanimichi <tanimichi@morality-line.com>
2021-09-07 23:07:48 +0900

    Ver3に合わせた実用的な対応 足りなかった実装などを追加

--- Assets/Editor/Scripts/Text2UtageExcelConverter.cs
+++ Assets/Editor/Scripts/Text2UtageExcelConverter.cs
@@ -1,72 +1,161 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using NPOI.SS.UserModel;
 using UnityEditor;
 using UnityEngine;
 
-namespace Utage {
-    public class Text2UtageExcelConverter : ScriptableObject {
+namespace Utage
+{
+	public class Text2UtageExcelConverter : ScriptableObject
+	{
 
-        [System.Serializable]
-        public class ExcelChapter : System.Object {
-            [SerializeField]
-            UnityEngine.Object _excelFile;
+		[System.Serializable]
+		public class ExcelChapter : System.Object
+		{
+			[SerializeField]
+			UnityEngine.Object _excelFile;
 
-            [SerializeField]
-            List<UnityEngine.Object> _textFiles;
+			[SerializeField]
+			List<UnityEngine.Object> _textFiles;
 
-            public UnityEngine.Object ExcelFile {
-                get { return _excelFile; }
+			public UnityEngine.Object ExcelFile
+			{
+				get { return _excelFile; }
 #if UNITY_EDITOR
-                set { _excelFile = value; }
+				set { _excelFile = value; }
 #endif
-            }
+			}
 
-            public List<UnityEngine.Object> TextFiles {
-                get { return _textFiles; }
+			public List<UnityEngine.Object> TextFiles
+			{
+				get { return _textFiles; }
 #if UNITY_EDITOR
-                set { _textFiles = value; }
+				set { _textFiles = value; }
 #endif
-            }
+			}
+		}
 
-        }
+		[System.Serializable]
+		public class ConvertFile
+		{
+			[SerializeField]
+			public UnityEngine.Object TextFile;
 
-        [SerializeField]
-        List<ExcelChapter> _excelChapters;
+			[SerializeField]
+			private string previouMd5Sum;
 
-        public List<ExcelChapter> ExcelChapters {
-            get {
+			public bool IsUpdate()
+			{
+				return previouMd5Sum != MD5Sum();
+			}
 
-                return _excelChapters;
-            }
+			public void SetMD5()
+			{
+				previouMd5Sum = MD5Sum();
+			}
+
+			private string MD5Sum()
+			{
+				var fullName = AssetDatabase.GetAssetPath(TextFile);
+				FileStream fs = new FileStream(fullName, FileMode.Open);
+				string md5sum = BitConverter.ToString(MD5.Create().ComputeHash(fs)).ToLower().Replace("-", "");
+				fs.Close();
+				return md5sum;
+			}
+		}
+
+		//		[HideInInspector]
+		public List<ConvertFile> converted = new List<ConvertFile>();
+
+		public void AddConverted(UnityEngine.Object textFile)
+		{
+			var convertedFlie = converted.Find(c => c.TextFile == textFile);
+			if (convertedFlie == null)
+			{
+				convertedFlie = new ConvertFile();
+				convertedFlie.TextFile = textFile;
+				converted.Add(convertedFlie);
+			}
+			convertedFlie.SetMD5();
+		}
+
+		public bool IsUpdate(UnityEngine.Object textFile)
+		{
+			if (!checkUpdateFile)
+			{
+				return true;
+			}
+
+			var convertedFlie = converted.Find(c => c.TextFile == textFile);
+			if (convertedFlie != null)
+			{
+				return convertedFlie.IsUpdate();
+			}
+
+			return true;
+		}
+
+
+		[SerializeField]
+		List<ExcelChapter> _excelChapters;
+
+		public List<ExcelChapter> ExcelChapters
+		{
+			get
+			{
+
+				return _excelChapters;
+			}
 #if UNITY_EDITOR
-            set { _excelChapters = value; }
+			set { _excelChapters = value; }
 #endif
-        }
+		}
 
-        public bool NovelMode {
-            get {
-                return novelMode;
-            }
+		public bool NovelMode
+		{
+			get
+			{
+				return novelMode;
+			}
 
-            set {
-                novelMode = value;
-            }
-        }
+			set
+			{
+				novelMode = value;
+			}
+		}
 
-        public Vector2Int TextMatrixXY {
-            get {
-                return textMatrixXY;
-            }
-        }
+		public int TextCount
+		{
+			get
+			{
+				return _excelChapters.SelectMany(c => c.TextFiles).Count();
+			}
+		}
 
-        [SerializeField, HeaderAttribute ("改行クリック待ちで前の文章を消さない")]
-        private bool novelMode = false;
+		public Vector2Int TextMatrixXY
+		{
+			get
+			{
+				return textMatrixXY;
+			}
+		}
 
-        [SerializeField, HeaderAttribute ("ノベル時の一画面の列文字数と行数")]
-        private Vector2Int textMatrixXY;
+		[SerializeField, HeaderAttribute("改行クリック待ちで前の文章を消さない")]
+		private bool novelMode = false;
 
-    }
+		[SerializeField, HeaderAttribute("ノベル時の一画面の列文字数と行数")]
+		private Vector2Int textMatrixXY;
+
+
+		[SerializeField, HeaderAttribute("テキストに更新がない場合は変換しない")]
+		private bool checkUpdateFile;
+
+		public bool CheckUpdateFile { get => checkUpdateFile; }
+
+	}
 
 }
--- Assets/Editor/Scripts/Text2UtageExcelUtil.cs
+++ Assets/Editor/Scripts/Text2UtageExcelUtil.cs
@@ -1,164 +1,276 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using NPOI.SS.UserModel;
 using UnityEngine;
 
-namespace Text2Utage {
-    public class Util {
-        public enum ColumnEnum {
-            Command = 0,
-            Arg1,
-            Arg2,
-            Arg3,
-            Arg4,
-            Arg5,
-            Arg6,
-            Text,
-            PageCtrl,
-            Voice,
-            WindowType,
-            English
-        }
+namespace Text2Utage
+{
+	public class Util
+	{
+		public enum ColumnEnum
+		{
+			Command = 0,
+			Arg1,
+			Arg2,
+			Arg3,
+			Arg4,
+			Arg5,
+			Arg6,
+			WaitType,
+			Text,
+			PageCtrl,
+			Voice,
+			WindowType,
+			English
+		}
 
-        public static bool isOneByteChar (char ch) {
-            var length = System.Text.Encoding.GetEncoding ("UTF-8").GetByteCount (new char[] { ch });
-            return length == 1;
-        }
+		public static bool isOneByteChar(char ch)
+		{
+			var length = System.Text.Encoding.GetEncoding("UTF-8").GetByteCount(new char[] { ch });
+			return length == 1;
+		}
 
-        public static bool isOneByteChar (string str) {
-            byte[] byteData = System.Text.Encoding.GetEncoding (932).GetBytes (str);
-            return byteData.Length == str.Length;
-        }
-    }
+		public static bool isOneByteChar(string str)
+		{
+			byte[] byteData = System.Text.Encoding.GetEncoding(932).GetBytes(str);
+			return byteData.Length == str.Length;
+		}
 
-    public class Sheet {
-        private ISheet targetSheet;
-        private bool novelMode;
+		public static bool isCommand(string str)
+		{
+			return str[0] == '@' || str[0] == '[';
+		}
+	}
 
-        private int rowCount;
+	public class Sheet
+	{
+		private ISheet targetSheet;
+		private bool novelMode;
 
-        public Sheet (ISheet seet, bool novel) {
-            targetSheet = seet;
-            novelMode = novel;
-            SetColumnHeader ();
-            rowCount = 1;
-        }
+		private int rowCount;
 
-        void SetColumnHeader () {
-            var row = targetSheet.CreateRow (0);
+		private bool isPreviousCharactorCommand;
 
-            string[] array = Enum.GetNames (typeof (Util.ColumnEnum));
-            for (int i = 0; i < array.Length; i++) {
-                string name = array[i];
-                var targetCell = row.CreateCell (i);
-                targetCell.SetCellValue (name);
-                Console.WriteLine (name);
-            }
-        }
+		public Sheet(ISheet seet, bool novel)
+		{
+			targetSheet = seet;
+			novelMode = novel;
+			SetColumnHeader();
+			rowCount = 1;
+		}
 
-        private bool IsArgTextInCommand (string[] commands) {
-            for (int i = 1; i < commands.Length; i++) {
-                if (commands[i].ToLower ().Contains ("arg")) return true;
-            }
-            return false;
-        }
+		void SetColumnHeader()
+		{
+			var row = targetSheet.CreateRow(0);
 
-        public void SetLine (string line) {
-            if (line == "") { return; }
+			var columnArray = Enum.GetNames(typeof(Util.ColumnEnum));
+			for (int i = 0; i < columnArray.Length; i++)
+			{
+				string name = columnArray[i];
+				var targetCell = row.CreateCell(i);
+				targetCell.SetCellValue(name);
+				Console.WriteLine(name);
+			}
+		}
 
-            var firstChar = line[0];
-            var targetRow = new Row (targetSheet.CreateRow (rowCount));
+		private bool IsArgTextInCommand(string[] commands)
+		{
+			for (int i = 0; i < commands.Length; i++)
+			{
+				return commands[i].ToLower().Contains("arg");
+			}
+			return false;
+		}
 
-            //コメントの場合
-            if (firstChar == ';' || firstChar == '/') {
-                return;
-            }
+		public void SetLine(string line)
+		{
+			if (line == "") { return; }
 
-            //文章の場合
-            if (!Util.isOneByteChar (line) && firstChar != '[' && firstChar != '@') {
-                targetRow.SetText (line, novelMode);
-                rowCount++;
-                return;
-            }
+			var firstChar = line[0];
+			Row targetRow;
 
-            //改行コマンドの場合、1つ前のPageCtrlを削除
-            if (targetRow.IsNewLineTag (line)) {
-                if (rowCount == 1) return;
+			if (isPreviousCharactorCommand)
+			{
+				targetRow = new Row(targetSheet.GetRow(targetSheet.LastRowNum));
+				isPreviousCharactorCommand = false;
+			}
+			else
+			{
+				targetRow = new Row(targetSheet.CreateRow(rowCount));
+			}
 
-                var beforeRow = targetSheet.GetRow (rowCount - 1);
-                var pageCtrlCell = beforeRow.CreateCell ((int) Util.ColumnEnum.PageCtrl);
-                pageCtrlCell.SetCellValue ("");
-                return;
-            }
+			//コメントの場合
+			if (firstChar == ';' || firstChar == '/')
+			{
+				return;
+			}
 
-            //ここまで来たらコマンド行とみなし、整形
-            var commandText = line.Trim ('[', ']', '@').Replace (" =", "=").Replace ("= ", "=").Replace ("  ", " ");
+			//コマンドではない
+			if (!Util.isCommand(line))
+			{
+				//キャラクターコマンドの場合
+				if (firstChar == '【')
+				{
+					var argsSpiritedText = line.Replace("【", "").Replace("】", "").Replace("  ", " ").Split(' ');
+					argsSpiritedText[0] = "arg1" + argsSpiritedText[0];
 
-            string[] splittedCommandText = commandText.Split (' ');
-            string[] commands = new string[6];
-            commands[0] = splittedCommandText[0];
+					var args = GetArgContentArray(argsSpiritedText);
+					//名前
+					args[0] = Regex.Match(line, "(?<=【).*?(?=】)").Value;
 
-            if (IsArgTextInCommand (splittedCommandText)) {
-                //Arg表記ありの場合、arg指定に合わせる
-                for (int i = 1; i < splittedCommandText.Length; i++) {
-                    for (int argNum = 1; argNum < 6; argNum++) {
-                        if (splittedCommandText[i].ToLower ().Contains ("arg" + argNum)) {
-                            commands[argNum] = splittedCommandText[i].Replace ("arg" + i + "=", "");
-                        }
-                    }
-                }
-            } else {
-                //Arg表記なしの場合、順に入れる
-                for (int i = 1; i < splittedCommandText.Length; i++) {
-                    if (splittedCommandText[i] == "") continue;
-                    commands[i] = splittedCommandText[i];
-                }
-            }
-            targetRow.SetCommand (commands);
+					targetRow.SetCommand("", args);
+					isPreviousCharactorCommand = true;
+					return;
+				}
 
-            rowCount++;
-        }
-    }
+				targetRow.SetText(line, novelMode);
+				rowCount++;
+				return;
+			}
 
-    public class Row {
-        private IRow targetRow;
+			//改行コマンドの場合、1つ前のPageCtrlを削除
+			if (targetRow.IsNewLineTag(line))
+			{
+				if (rowCount == 1) return;
 
-        public Row (IRow row) {
-            targetRow = row;
-        }
+				var beforeRow = targetSheet.GetRow(rowCount - 1);
+				var pageCtrlCell = beforeRow.CreateCell((int)Util.ColumnEnum.PageCtrl);
+				pageCtrlCell.SetCellValue("");
+				return;
+			}
 
-        private void SetPageCtrlInputBr (IRow targetRow) {
-            var pageCtrlCell = targetRow.CreateCell ((int) Util.ColumnEnum.PageCtrl);
-            pageCtrlCell.SetCellValue ("InputBr");
-        }
+			//ここまで来たらコマンド行とみなし、整形
+			var commandText = line.Trim('[', ']', '@').Replace(" =", "=").Replace("= ", "=").Replace("  ", " ");
 
-        public bool IsNewLineTag (string line) {
-            return (line.IndexOf ("[p]") > -1 || line.IndexOf ("@p") > -1);
-        }
+			string[] splittedCommandText = commandText.Split(' ');
+			string[] argSplittedText = new string[splittedCommandText.Length - 1];
 
-        public void SetText (string text, bool novelMode = false) {
-            var cell = targetRow.CreateCell ((int) Util.ColumnEnum.Text);
+			Array.Copy(splittedCommandText, 1, argSplittedText, 0, splittedCommandText.Length - 1);
 
-            if (novelMode && !IsNewLineTag (text)) {
-                SetPageCtrlInputBr (targetRow);
-            }
 
-            cell.SetCellValue (text.Replace ("[p]", "").Replace ("@p", ""));
-        }
+			if (splittedCommandText[0] == "Tween")
+			{
 
-        public void SetCommand (string[] commands) {
-            var commandCell = targetRow.CreateCell ((int) Util.ColumnEnum.Command);
-            commandCell.SetCellValue (commands[0]);
+			}
 
-            for (int i = 1; i < commands.Length; i++) {
-                if (!string.IsNullOrEmpty (commands[i])) {
-                    var targetCell = targetRow.CreateCell (i);
-                    targetCell.SetCellValue (commands[i]);
-                }
-            }
-        }
+			targetRow.SetCommand(splittedCommandText[0], GetArgContentArray(argSplittedText));
 
-    }
+			rowCount++;
+		}
+
+		private string[] GetArgContentArray(string[] splittedArgText)
+		{
+			string[] args = new string[7];
+			if (splittedArgText == null)
+			{
+				return args;
+			}
+
+			if (IsArgTextInCommand(splittedArgText))
+			{
+				//Arg表記ありの場合、arg指定に合わせる
+				for (int i = 0; i < splittedArgText.Length; i++)
+				{
+					for (int argNum = 0; argNum < 6; argNum++)
+					{
+						if (splittedArgText[i].ToLower().Contains("arg" + (argNum + 1)))
+						{
+							args[argNum] = splittedArgText[i]
+								.Replace("arg" + (argNum + 1) + "=", "")
+								.Replace("Arg" + (argNum + 1) + "=", "")
+								.Replace("ARG" + (argNum + 1) + "=", "");
+
+							//Tween用
+							if (args[argNum].Contains("{"))
+							{
+								args[argNum] = args[argNum]
+									.Replace("{", "")
+									.Replace("}", "")
+									.Replace(",", " ");
+							}
+						}
+
+						if (splittedArgText[i].ToLower().Contains(Util.ColumnEnum.WaitType.ToString().ToLower()))
+						{
+							args[6] = splittedArgText[i].Replace(Util.ColumnEnum.WaitType.ToString() + "=", "");
+						}
+					}
+				}
+			}
+			else
+			{
+				//Arg表記なしの場合、順に入れる
+				for (int i = 1; i < splittedArgText.Length; i++)
+				{
+					if (splittedArgText[i] == "") continue;
+					args[i] = splittedArgText[i];
+				}
+			}
+
+
+
+			return args;
+		}
+	}
+
+
+
+	public class Row
+	{
+		private IRow targetRow;
+
+		public Row(IRow row)
+		{
+			targetRow = row;
+		}
+
+		private void SetPageCtrlInputBr(IRow targetRow)
+		{
+			var pageCtrlCell = targetRow.CreateCell((int)Util.ColumnEnum.PageCtrl);
+			pageCtrlCell.SetCellValue("InputBr");
+		}
+
+		public bool IsNewLineTag(string line)
+		{
+			return (line.IndexOf("[p]") > -1 || line.IndexOf("@p") > -1);
+		}
+
+		public void SetText(string text, bool novelMode = false)
+		{
+			var cell = targetRow.CreateCell((int)Util.ColumnEnum.Text);
+
+			if (novelMode && !IsNewLineTag(text))
+			{
+				SetPageCtrlInputBr(targetRow);
+			}
+
+			cell.SetCellValue(text.Replace("[p]", "").Replace("@p", ""));
+		}
+
+		public void SetCommand(string command, string[] args)
+		{
+			var commandCell = targetRow.CreateCell((int)Util.ColumnEnum.Command);
+
+			//宴の仕様上、キャラクターだけを表示する命令はコマンドが空白
+			if (command.ToLower() == "characteron")
+			{
+				command = "";
+			}
+
+			commandCell.SetCellValue(command);
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				if (!string.IsNullOrEmpty(args[i]))
+				{
+					var targetCell = targetRow.CreateCell(i + 1);
+					targetCell.SetCellValue(args[i]);
+				}
+			}
+		}
+
+	}
 
 }
